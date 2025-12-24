1b8b86c4f81d9c0c5ed274fd4fc2bea8ec6c6eb7
Yuzora-Yu <rikai.829@gmail.com>
2025-12-25 00:27:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prisma-abyss-v1";
+const CACHE_NAME = "prisma-abyss-v1.01";
 const FILES_TO_CACHE = [
   "./", // ルート
   "main.html",
@@ -49,3 +49,4 @@ self.addEventListener("fetch", (e) => {
     })
   );
 });
+
